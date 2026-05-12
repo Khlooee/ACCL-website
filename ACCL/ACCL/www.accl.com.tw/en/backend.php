cd9cf8d5fb3c32c08f53ef9a4a0ee9ed23b939f8
Khlooee <129675953+Khlooee@users.noreply.github.com>
2026-05-12 11:18:09 +0000

    说明

---
+++ ACCL/ACCL/www.accl.com.tw/en/backend.php
@@ -0,0 +1,104 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+    <channel>
+        <title>ACCL</title>
+        <link>
+        https://www.accl.com.tw/en/</link>
+        <description></description>
+        <lastBuildDate>Sat, 09 May 2026 19:10:41 +1600</lastBuildDate>
+        <docs>http://backend.userland.com/rss/</docs>
+        <generator>XOOPS</generator>
+        <category>News</category>
+        <managingEditor>admin at nc dot com dot tw</managingEditor>
+        <webMaster>admin at nc dot com dot tw</webMaster>
+        <language>en</language>
+                    <image>
+                <title>ACCL</title>
+                <url>https://www.accl.com.tw/en/images/logo.png</url>
+                <link>
+                https://www.accl.com.tw/en/</link>
+                <width>144</width>
+                <height>48</height>
+            </image>
+                            <item>
+                <title>Corporate Governance</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=38</link>
+                <description>&lt;p&gt;&lt;span style=&quot;font-size: large;&quot;&gt;&lt;strong&gt;Major Internal Policies&lt;/strong&gt;&lt;/span&gt;&lt;/p&gt;&lt;table class=&quot;cadch_table07&quot; style=&quot;width: 100%;&quot;&gt;&lt;tbody&gt;&lt;tr style=&quot;background: #FAFAFA;&quot;&gt;&lt;td&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Articles of Incorporation&lt;/span&gt;&lt;/td&gt;&lt;td style=&quot;text-align: center;&quot;&gt;&lt;a href=&quot;../../../tw/uploads/filelist/1000/2/1736305140_a770981e9f456be.pdf&quot; title=&quot;Articles of Incorporation&quot; target=&quot;_blank&quot;&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;&lt;img height=&quot;26&quot; src=&quot;../../../tw/uploads/filelist/1000/2/1622105097_a77097d5c09ee1d.png&quot; width=&quot;26&quot; /&gt;&lt;/span&gt;&lt;/a&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/tbody&gt;&lt;/table&gt;</description>
+                <pubDate>Wed, 08 Jan 2025 02:42:58 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=38</guid>
+            </item>
+                    <item>
+                <title>ACCL Sustainability Report</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=37</link>
+                <description>&lt;div style=&quot;width: fit-content;&quot;&gt;&lt;a href=&quot;../../../tw/uploads/filelist/1000/2/1723601019_a770981107bdf02.pdf&quot; target=&quot;_blank&quot;&gt;&lt;img height=&quot;375&quot; src=&quot;../../../tw/uploads/filelist/1000/2/1723529337_a770981f87930df.png&quot; width=&quot;250&quot; /&gt;&lt;/a&gt;&lt;div style=&quot;font-size: medium; text-align: center;&quot;&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;2023 Sustainability Report&lt;/span&gt;&lt;/div&gt;&lt;/div&gt;</description>
+                <pubDate>Tue, 13 Aug 2024 06:12:40 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=37</guid>
+            </item>
+                    <item>
+                <title>Contractors Management</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=18</link>
+                <description>&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;ACCL requires contractors to comply with occupational safety and health related laws and regulations, and to improve their safety and health concepts and knowledge. Implementation of audits: In addition to observing occupational safety and health related laws and regulations, if violations or abnormalities are found in the inspection audit, improvement is required to avoid danger in the operation.&lt;/span&gt;&lt;/p&gt;</description>
+                <pubDate>Thu, 25 Feb 2021 10:10:00 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=18</guid>
+            </item>
+                    <item>
+                <title>Learning in ACCL</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=35</link>
+                <description>&lt;p&gt;&lt;span style=&quot;font-size: large;&quot;&gt;&lt;strong&gt;● Training System Introduction&lt;/strong&gt;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;In order to respond to the rapid changes in industrial technology and to ensure the development of employees&#039; talents and careers, and to achieve the company&#039;s operating goals, the company has set employee learning and development as a key human resource management project. Provide employees with an open and diversified learning environment. Colleagues can constantly challenge their own growth limits through internal/external training, OJT, online learning from SME Internet University, and guidance from supervisors and peers; at the same time, through new recruits and professional functions , Supervisors, general education courses and self-inspired training system, so that employees get the greatest satisfaction. On the other hand, through grade/level planning and project assignment, the careers and careers of colleagues can be combined with each other, so that they can enjoy the joy of knowing and growing together and create a better future.&lt;/span&gt;&lt;/p&gt;</description>
+                <pubDate>Thu, 25 Feb 2021 10:08:14 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=35</guid>
+            </item>
+                    <item>
+                <title>Working at ACCL</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=34</link>
+                <description>&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: large;&quot;&gt;● Salary Policy&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;ACCL has a diversified and competitive overall salary, regularly reviews the salary level and flexibly designs a regular and irregular bonus system for each position; creates a dividend incentive plan that improves company operations, team and individual performance, and shares the company with all employees Profitable surplus.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&amp;nbsp;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: large;&quot;&gt;●&amp;nbsp;Welfare System&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;ul style=&quot;list-style-type: square;&quot;&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Employee dividends and stock subscriptions, annual festivals and performance bonuses&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Excellent staff/senior staff rewards&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Excellent new-hired introduction, retention, long-term bonus&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Labor insurance, national health insurance and group accident medical insurance.&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Regular free employee health check&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Free meals, staff uniforms/work shoes&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Staff dormitory (free for the first three months)&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Provide employee parking spaces for cars and locomotives&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;The Employee Welfare Committee implements employee welfare measures (group activities such as three festival gifts, birthday gifts, weddings and funerals, subsidies for injuries and illnesses, maternity allowances, employee travel, dinner parties, and family parent-child days).&lt;/span&gt;&lt;/li&gt;&lt;/ul&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;&amp;nbsp;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: large;&quot;&gt;●&amp;nbsp;Performance Management System&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;ACCL is a professional manager company with a transparent, fair, just and open performance management system. The appraisal is handled every six months. Employees can set personal annual work goals and plans through face-to-face communication with their supervisors, and regularly review the results of the goal implementation, and achieve their work goals with immediate feedback and suggestions from the supervisor.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;&amp;nbsp;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: large;&quot;&gt;●&amp;nbsp;Career Development&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;The promotion and evaluation work is an important task for the cultivation and development of Allied talents. According to the needs of organizational development, supervisors and internal reviewers at all levels need to evaluate whether the assesse meets the characteristics of the promotion position. Objective and prudent attitude evaluation, provide diversified promotion and development opportunities, and cultivate high-quality talents in a planned way to enhance ACCL&amp;rsquo;s human assets and shape a service-oriented corporate culture.&lt;/span&gt;&lt;/p&gt;</description>
+                <pubDate>Thu, 25 Feb 2021 10:08:01 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=34</guid>
+            </item>
+                    <item>
+                <title>Join ACCL</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=33</link>
+                <description>&lt;p&gt;&lt;span style=&quot;font-size: large;&quot;&gt;&lt;strong&gt;●&amp;nbsp;&lt;span style=&quot;color: #008000;&quot;&gt;&lt;a href=&quot;https://www.104.com.tw/company/1543bto8?jobsource=checkc&quot; target=&quot;_blank&quot;&gt;&lt;span style=&quot;color: #008000;&quot;&gt;HOT JOBS&lt;/span&gt;&lt;/a&gt;&lt;/span&gt;&lt;/strong&gt;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: large;&quot;&gt;&lt;strong&gt;● Recruitment process&lt;/strong&gt;&lt;/span&gt;&lt;/p&gt;&lt;ol&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Job Search&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Please go to &quot;Latest Jobs&quot; to search for a job that suits you and submit your resume directly.&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Resume Screening&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;The preliminary screening will be carried out with reference to your educational background, professional skills, work experience, etc., and the appropriate candidates will be notified by telephone about the follow-up interview. If there is no suitable vacancy at present, your information will be put into the exclusive talent pool, and when a suitable vacancy is available, we will actively contact you.&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Interview Conducted&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Before the interview, we will arrange a professional and suitability test; during the interview phase, an interview with the supervisor will be arranged. At this time, you can further understand the work content and system; within 14 working days after the interview, the result will be notified by phone or email.&lt;/span&gt;&lt;/li&gt;&lt;li&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Registration for Employment&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;After going through the rigorous interview procedure, we will notify the admitted applicants by phone and send an electronic admission notice. You are welcome to join the ranks of Allied and create a better future with us.&lt;/span&gt;&lt;/li&gt;&lt;/ol&gt;</description>
+                <pubDate>Thu, 25 Feb 2021 10:07:49 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=33</guid>
+            </item>
+                    <item>
+                <title>Shareholder Services</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=28</link>
+                <description>&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: large;&quot;&gt;● Shareholders&#039; Meeting&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;2025 Shareholders&#039; Meeting&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Date：26 May 2025&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Time：9:00 am&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Location：B1, No.128, Gong 2nd Rd., Wulin Vil., Longtan Dist., Taoyuan 32559, Taiwan R.O.C.&lt;/span&gt;&lt;/p&gt;</description>
+                <pubDate>Thu, 25 Feb 2021 10:05:43 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=28</guid>
+            </item>
+                    <item>
+                <title>Financial Reports</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=20</link>
+                <description>&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;For the financial statements please click &lt;span style=&quot;color: #008000;&quot;&gt;&lt;a href=&quot;https://emops.twse.com.tw/server-java/t58query&quot; style=&quot;color: #0000cc; text-decoration: underline; font-weight: bold;&quot; target=&quot;_blank&quot;&gt;here&lt;/a&gt;&lt;/span&gt; to get the information on Market Observation Post System (MOPS).&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Company Code：8155&lt;/span&gt;&lt;/p&gt;</description>
+                <pubDate>Thu, 25 Feb 2021 10:01:52 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=20</guid>
+            </item>
+                    <item>
+                <title>Charity Care</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=19</link>
+                <description>&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Love Activities&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;ACCL has participated in various peace-of-mind activities and donations since 2013, including the &quot;Children&#039;s Dream Flying Project&quot; of the Xu Chaoying Charity Foundation to care for disadvantaged children, the World Vision, the Formosa Fun Coast water park blaze, and the TPCA Environment Foundation.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;&amp;nbsp;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Community Participation&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Establish friendly relations with the neighbors, regularly invite local and foreign colleagues to clean the surrounding environment of the factory, maintain the cleanliness of the neighborhood, promote environmental protection and cleanliness activities, and give back to Wulin Vil. environmental protection resource recycling vehicle donations.&lt;/span&gt;&lt;/p&gt;</description>
+                <pubDate>Thu, 25 Feb 2021 10:01:19 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=19</guid>
+            </item>
+                    <item>
+                <title>Safety and Health</title>
+                <link>
+                https://www.accl.com.tw/en/modules/news/article.php?storyid=17</link>
+                <description>&lt;p&gt;&lt;span style=&quot;font-size: large;&quot;&gt;&lt;strong&gt;A. Workplace Safety&lt;/strong&gt;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;Occupational Safety and Health System Certification&lt;/span&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-size: medium;&quot;&gt;ACCL obtained ISO45001 occupational safety and health system certification through system and source management.&lt;/span&gt;&lt;/p&gt;</description>
+                <pubDate>Thu, 25 Feb 2021 10:00:28 +1600</pubDate>
+                <guid>https://www.accl.com.tw/en/modules/news/article.php?storyid=17</guid>
+            </item>
+            </channel>
+</rss>
